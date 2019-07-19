b959374a7e183be8ce272945e1ae938b6dbb2ca4
tdd <tdd_style@163.com>
2019-07-19 19:12:25 +0800

    add js to module plugin

--- src/plugins/es52es5.js
+++ src/plugins/es52es5.js
@@ -7,7 +7,7 @@ const babel = require("@babel/core");
 const util = require('../ni/util');
 
 exports.modify = (filename,relativePath,bcfg,pcfg,callback) => {
-    let data = fs.readFileSync(filename),info = {path: relativePath.replace(".es6",".js")};
+    let data = fs.readFileSync(filename),info = {path: relativePath.replace(".es5",".js")};
     try{
         data = data.toString();
         info.sign = util.createHash(data);
@@ -16,19 +16,19 @@ exports.modify = (filename,relativePath,bcfg,pcfg,callback) => {
         }
         info.depends = util.findDepends(data,info.path);
         data = {code:data};
-        data.es5 = babel.transform(data.code, {
-            filename,
-            presets: [["@babel/preset-env",{"targets": "> 0.25%, not dead"}]],
-            plugins:[["@babel/plugin-proposal-class-properties",{"loose": true,"modules":"commonjs"
-        }]]
-        })
+        // data.es5 = babel.transform(data.code, {
+        //     filename,
+        //     presets: [["@babel/preset-env",{"targets": "> 0.25%, not dead"}]],
+        //     plugins:[["@babel/plugin-proposal-class-properties",{"loose": true,"modules":"commonjs"
+        // }]]
+        // })
         // data.es5 = babel.transform(data.es6.code,{
         //     presets: [["@babel/preset-env",{"targets": "> 0.25%, not dead"}]]
         // })
-        data.es5.code = findRequire(bcfg.distAbsolute,path.join(bcfg.distAbsolute,info.path),data.es5.code);
-        data.es5.code = `define("${path.normalize(relativePath).replace(".es6","").replace(/\\/gi,"/").replace("/","")}",function(require,exports,module){\n${data.es5.code}\n})`;
-        info.size = data.es5.code.length;
-        fs.writeFileSync(`${path.join(bcfg.distAbsolute,info.path)}`,data.es5.code,"utf8");
+        data.code = findRequire(bcfg.distAbsolute,path.join(bcfg.distAbsolute,info.path),data.code);
+        data.code = `define("${path.normalize(relativePath).replace(".es5","").replace(/\\/gi,"/").replace("/","")}",function(require,exports,module){\n${data.code}\n})`;
+        info.size = data.code.length;
+        fs.writeFileSync(`${path.join(bcfg.distAbsolute,info.path)}`,data.code,"utf8");
         callback(info);
     }catch(e){
         console.log(e,filename);
